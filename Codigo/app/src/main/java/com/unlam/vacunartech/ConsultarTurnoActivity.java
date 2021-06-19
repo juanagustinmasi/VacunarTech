80bd882ebd506d42758eed7e9e17fb43722b32bf
Juan Masi <juan.2728@hotmail.com>
2021-06-19 15:47:21 -0300

    ConsultarActivity

---
+++ Codigo/app/src/main/java/com/unlam/vacunartech/ConsultarTurnoActivity.java
@@ -0,0 +1,14 @@
+package com.unlam.vacunartech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultarTurnoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_turno);
+    }
+}
